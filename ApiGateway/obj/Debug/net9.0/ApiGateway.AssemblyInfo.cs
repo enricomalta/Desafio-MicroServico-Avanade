3c74625c9732b7d699abc0ef4c1fc958c082e34e
enricomalta <enrico.malta1@gmail.com>
2025-09-19 16:28:10 -0300

    add .gitignore

--- ApiGateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a851568ff5b40a303ef3d48bf973ccd665d511")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ebc9ea9070cd503a36357adbb863246296c20f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
